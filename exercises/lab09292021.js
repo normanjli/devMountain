ede82c9890eaf05838dc6bb39b581c502a994103
Norman Li <normanjli@gmail.com>
2021-09-28 14:02:45 -0600

    Added solution to porblem 8

--- exercises/lab09292021.js
+++ exercises/lab09292021.js
@@ -118,8 +118,17 @@ var score = 74
 */
 
 //Code Here
-
-
+if (score >= 90){
+  console.log(`A`)
+}else if (score <90 && score >=80 ){
+  console.log(`B`)
+}else if (score <80 && score >=70 ){
+  console.log(`C`)
+}else if (score <70 && score >=60 ){
+  console.log(`D`)
+}else if (score <=59 ){
+  console.log(`F`)
+}
 ////////// Intermediate Problems //////////
 
 ////////// PROBLEM 9 //////////
