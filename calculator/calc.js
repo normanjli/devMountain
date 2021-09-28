74a2df26c9f19bd13f35c0475778a3bab53f8ec0
Norman Li <normanjli@gmail.com>
2021-09-28 11:36:43 -0600

    added prefix calculations for two numbers

--- calculator/calc.js
+++ calculator/calc.js
@@ -8,13 +8,33 @@ const reader = readline.createInterface({
 
 
 reader.question("What would you like to calculate?", function(input){
-	tokens = input.split(' ');
+	let tokens = input.split(',');
+	//console.log (tokens)
+	let mathSymbol = tokens[0];
+	let num1 = Number(tokens[1]);
+	let num2 = Number(tokens[2]);
+
+	if (mathSymbol === `+`) {
+		console.log (num1+num2);
+
+	}else if (mathSymbol === `-`){
+		console.log(num1-num2);
 	
-	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
+	}else if (mathSymbol === `*`){
+		console.log(num1*num2);
+	
+	}else if (mathSymbol === `/`){
+		console.log(num1/num2);
+
+	}else if (mathSymbol === `**`){
+		console.log(num1**.5);
+	
+	}else{
+		console.log (`you broke it, try entering input starting with operator then numerals seperated with commas`);
+	}
 
 
+	//console.log (num1, num2, mathSymbol)
 	// This line closes the connection to the command line interface.
 	reader.close()
 
