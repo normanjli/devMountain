2f23a3907337036f1381c301e768df04eadecac0
Norman Li <normanjli@gmail.com>
2021-09-28 09:36:28 -0600

    fix code to work

--- codechallenges/maxOfTwo.js
+++ codechallenges/maxOfTwo.js
@@ -1,14 +1,12 @@
-let x = "string"
+let x = 1
 let y = 2
-let z = (typeof (x) && typeof(y))
+
 // Write your code below this line.
-console.log(z)
+
 if (x == y){
     console.log("The values are the same.");
 }else if (x > y){
     console.log(x);
-}else if(y > x){
-    console.log(y);
 }else{
-    console.log ("itsbroke")
+    console.log(y);
 }
