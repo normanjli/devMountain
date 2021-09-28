0c222b7ecdc75b3d5d17f99c4c6937993e87dc4f
Norman Li <normanjli@gmail.com>
2021-09-28 15:44:54 -0600

    added check for final challenge

--- exercises/lab09292021.js
+++ exercises/lab09292021.js
@@ -277,15 +277,21 @@ switch (letterGrade){
   16
   ...
 */
+let divisByThree = []
+let divisByFive = []
+let divisByThreeandFive = []
 for (xiv = 1; xiv <=100; xiv++){
   if (xiv%3 == 0 && xiv%5 ==0){
     console.log(`Devmountain`)
+    divisByThreeandFive.push(xiv)
   }else if (xiv%5 == 0 && xiv%3 != 0){
     console.log(`Mountain`)
+    divisByFive.push(xiv)
   }else if (xiv%3 == 0 && xiv%5 != 0){
     console.log(`Dev`)
+    divisByThree.push(xiv)
   }else{
     console.log(xiv)
   }
-}
+}console.log(divisByThreeandFive, divisByThree, divisByFive)//verifies i am sorting values correctly
 //Code Here
