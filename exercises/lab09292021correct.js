dc2d199b5114271ac2f311c5e812d42dc2a181c5
Norman Li <normanjli@gmail.com>
2021-09-29 13:38:58 -0600

    solution 11

--- exercises/lab09292021correct.js
+++ exercises/lab09292021correct.js
@@ -147,7 +147,12 @@ if (changeMyMind === true) {
 
 //CODE HERE
 
-
+if (changeMyMind !== true){
+  changeMyMind = true
+}else{
+  console.log(`something broke`)
+}
+  console.log (changeMyMind)
 
 ////////// ADVANCED PROBLEMS //////////
 
@@ -157,3 +162,9 @@ let z = 5
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+while(z>0){
+  for(let i = z; i>0; i--) {
+  console.log(i)
+  }
+  z--
+}
