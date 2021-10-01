eae368a2a3db56f16768f3379bee5557a847d191
Norman Li <normanjli@gmail.com>
2021-10-01 07:30:53 -0600

    solutions to variables

--- assessments/week1/variables.js
+++ assessments/week1/variables.js
@@ -2,17 +2,18 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
-
+let myName = `Norman Li`
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+let faveNum = 8
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+let lovesCode = true
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
@@ -21,6 +22,11 @@
 */
 
 //CODE HERE
+if (faveNum === 15){
+  console.log(`lucky 13!`)
+}else{
+  console.log(`not lucky 13`)
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +34,6 @@
 */
 
 //CODE HERE
+for (let i = 0; i<5; i++){
+  console.log(faveNum)
+}
