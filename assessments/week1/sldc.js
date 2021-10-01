f1c11f8f2b389f629ebe9190011fcaf4d8a2fe82
Norman Li <normanjli@gmail.com>
2021-10-01 07:27:32 -0600

    sdlc answers

--- assessments/week1/sldc.js
+++ assessments/week1/sldc.js
@@ -5,15 +5,20 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
-
+// Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+let planning = `determine what role the program needs to fill, what the needs are for the remainder of the development lifecycle, and how many resources to allocate for the program`
+let analysis = `determine what functions are required for the program to fulfill the needs as determined in the planning step, determine what the minimun to market testing standards need to be`
+let design = `design the overall framework and individual functions of the program`
+let Implementation = `bringing the individual components which were made in the previous step and bring them together in one program`
+let Testing = `testing the progrm following integration to ensure program has robust function and will work for most consumers`
+let Maintain = `continuous maintanence of program to ensure robust function for novel use cases not discovered in the testing step or adding additional features as requested by consumer`
