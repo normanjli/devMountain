5c0c818585afde6d03790f940768933e6297cd4e
Norman Li <normanjli@gmail.com>
2021-10-06 07:14:46 -0600

    added solution

--- codechallenges/hasUniqueChars.js
+++ codechallenges/hasUniqueChars.js
@@ -1,1 +1,14 @@
 // Write your code below
+function hasUnique(str){
+
+let array = str.split(``)
+for (let i = 0; i < array.length; i++ ){
+    for( let j = i +1; j < array.length; j++){
+        if (array[i] === array[j]){
+            return false
+        }
+    }
+}return true
+}
+
+console.log(hasUnique(`Bob`))
