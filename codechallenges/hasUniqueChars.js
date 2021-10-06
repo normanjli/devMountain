970878e5912f43d8a01bbd05739486e918b9c0e2
Norman Li <normanjli@gmail.com>
2021-10-06 08:14:33 -0600

    added solutions using sets

--- codechallenges/hasUniqueChars.js
+++ codechallenges/hasUniqueChars.js
@@ -1,6 +1,5 @@
 // Write your code below
 function hasUnique(str){
-
 let array = str.split(``)
 for (let i = 0; i < array.length; i++ ){
     for( let j = i +1; j < array.length; j++){
@@ -8,10 +7,10 @@ for (let i = 0; i < array.length; i++ ){
             return false
         }
     }
-}return true
+    }return true
 }
 
-console.log(hasUnique(`Bob`))
+
 
 function hasUniqueSet(str){
     let set = new Set (str)
@@ -19,4 +18,20 @@ function hasUniqueSet(str){
         return true
     }return false
 }
-console.log(hasUniqueSet(`Bob`))
+
+
+//Andrew
+function hasUniqueChars (str) {
+    let chars = {}
+    for(let i = 0; i < str.length; i++){
+      console.log(chars)
+      let letter = str[i]
+      console.log(letter, chars[letter])
+      if (chars[letter]) {
+        return false
+      }
+      chars[letter] = (chars[letter] || 0) + 1
+    } return true
+  }
+  
+  hasUniqueChars('The quick brown fox jupmed over the lazy dog.')
