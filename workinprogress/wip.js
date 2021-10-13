05302ed837663143c17c4a323871b0de9f590c25
Norman Li <normanjli@gmail.com>
2021-10-13 11:39:46 -0600

    working now

--- workinprogress/wip.js
+++ workinprogress/wip.js
@@ -1,91 +1,103 @@
-function alertFx() {
-    alert(`2`)
-}
-
-function calc(){
-    let tokens = prompt(`enter operation starting with operator then values separated with commas`);
-    tokens = tokens.split(`,`)
-    let mathSymbol = tokens.shift()
-    let convTokens = []
-    for (let i = 0; i < tokens.length; i++){
-        convTokens.push(Number(tokens[i]))
+const calc = () => {
+  let tokens = prompt(
+    `enter operation starting with operator then values separated with commas`
+  );
+  tokens = tokens.split(`,`);
+  let mathSymbol = tokens.shift();
+  let convTokens = [];
+  for (let i = 0; i < tokens.length; i++) {
+    convTokens.push(Number(tokens[i]));
+  }
+  // alert(convTokens)
+  let ans = 1;
+  let sqrt = [];
+  if (mathSymbol === `+`) {
+    for (let i = 0; i < convTokens.length; i++) {
+      ans += convTokens[i];
     }
-    // alert(convTokens)
-    let ans = 1
-    let sqrt = []
-	if (mathSymbol === `+`) {
-        for (let i = 0; i < convTokens.length; i++){
-            ans += convTokens[i]
-        }alert(ans-1)
-	}else if (mathSymbol === `-`){
-		for (let i = 0; i < convTokens.length; i++){
-            ans -= convTokens[i]
-        }alert(ans-1)
-	}else if (mathSymbol === `*`){
-		for (let i = 0; i < convTokens.length; i++){
-            ans *= convTokens[i]
-        }alert(ans)
-	}else if (mathSymbol === `/`){
-        for (let i = 0; i < convTokens.length; i++){
-            ans /= convTokens[i]
-        }alert(ans)
-	}else if (mathSymbol === `^/`){
-		for (let i = 0; i < convTokens.length; i++){
-             sqrt.push(convTokens[i]**.5)
-            }if (convTokens.length > 1){
-                alert(`the answers are ${sqrt}`)
-            }else{
-                alert(`the answer is ${sqrt}`)
-            }
-	}else{
-		alert (`you broke it, try entering input starting with operator then numerals seperated with commas`);
-	}
+    alert(ans - 1);
+  } else if (mathSymbol === `-`) {
+    for (let i = 0; i < convTokens.length; i++) {
+      ans -= convTokens[i];
+    }
+    alert(ans - 1);
+  } else if (mathSymbol === `*`) {
+    for (let i = 0; i < convTokens.length; i++) {
+      ans *= convTokens[i];
+    }
+    alert(ans);
+  } else if (mathSymbol === `/`) {
+    for (let i = 0; i < convTokens.length; i++) {
+      ans /= convTokens[i];
+    }
+    alert(ans);
+  } else if (mathSymbol === `^/`) {
+    for (let i = 0; i < convTokens.length; i++) {
+      sqrt.push(convTokens[i] ** 0.5);
+    }
+    if (convTokens.length > 1) {
+      alert(`the answers are ${sqrt}`);
+    } else {
+      alert(`the answer is ${sqrt}`);
+    }
+  } else {
+    alert(
+      `you broke it, try entering input starting with operator then numerals seperated with commas`
+    );
+  }
 
-
-	//console.log (num1, num2, mathSymbol)
-	// This line closes the connection to the command line interface
+  //console.log (num1, num2, mathSymbol)
+  // This line closes the connection to the command line interface
 };
 
-function entry() {
-    console.log(`running`)
-    alert(`running`)
-    let mathSymbol = document.getElementById("mathSymbol")
-    let tokens = document.getElementById(`tokens`)
-    console.log(tokens)
-    console.log(mathSymbol)
-//     tokens = tokens.split(`,`)
-//     let convTokens = []
-//     for (let i = 0; i < tokens.length; i++){
-//         convTokens.push(Number(tokens[i]))
-//     }
-//     // alert(convTokens)
-//     let ans = 1
-//     let sqrt = []
-// 	if (mathSymbol === `+`) {
-//         for (let i = 0; i < convTokens.length; i++){
-//             ans += convTokens[i]
-//         }alert(ans-1)
-// 	}else if (mathSymbol === `-`){
-// 		for (let i = 0; i < convTokens.length; i++){
-//             ans -= convTokens[i]
-//         }alert(ans-1)
-// 	}else if (mathSymbol === `*`){
-// 		for (let i = 0; i < convTokens.length; i++){
-//             ans *= convTokens[i]
-//         }alert(ans)
-// 	}else if (mathSymbol === `/`){
-//         for (let i = 0; i < convTokens.length; i++){
-//             ans /= convTokens[i]
-//         }alert(ans)
-// 	}else if (mathSymbol === `^/`){
-// 		for (let i = 0; i < convTokens.length; i++){
-//              sqrt.push(convTokens[i]**.5)
-//             }if (convTokens.length > 1){
-//                 alert(`the answers are ${sqrt}`)
-//             }else{
-//                 alert(`the answer is ${sqrt}`)
-//             }
-// 	}else{
-// 		alert (`you broke it, try entering input starting with operator then numerals seperated with commas`);
-// 	}
-}
+const entry = (e) => {
+  e.preventDefault();
+  document.querySelector(`p`).textContent = ``;
+  let ans = 1;
+  let sqrt = [];
+  let mathSymbol = document.getElementById("mathSymbol").value;
+  let tokens = document.getElementById(`tokens`).value;
+  tokens = tokens.split(`,`);
+  let convTokens = [];
+  for (let i = 0; i < tokens.length; i++) {
+    convTokens.push(Number(tokens[i]));
+  }
+  // alert(convTokens)
+  if (mathSymbol === `+`) {
+    for (let i = 0; i < convTokens.length; i++) {
+      ans += convTokens[i];
+    }
+    document.querySelector(`p`).textContent = ans - 1;
+  } else if (mathSymbol === `-`) {
+    for (let i = 0; i < convTokens.length; i++) {
+      ans -= convTokens[i];
+    }
+    document.querySelector(`p`).textContent = ans - 1;
+  } else if (mathSymbol === `*`) {
+    for (let i = 0; i < convTokens.length; i++) {
+      ans *= convTokens[i];
+    }
+    document.querySelector(`p`).textContent = ans;
+  } else if (mathSymbol === `/`) {
+    for (let i = 0; i < convTokens.length; i++) {
+      ans /= convTokens[i];
+    }
+    document.querySelector(`p`).textContent = ans;
+  } else if (mathSymbol === `^/`) {
+    for (let i = 0; i < convTokens.length; i++) {
+      sqrt.push(convTokens[i] ** 0.5);
+    }
+    if (convTokens.length > 1) {
+      document.querySelector(`p`).textContent = `The sqrt values are ${sqrt}`;
+    } else {
+      document.querySelector(`p`).textContent = `the answer is ${sqrt}`;
+    }
+  } else {
+    alert(
+      `you broke it, try entering input starting with operator then numerals seperated with commas`
+    );
+  }
+  document.getElementById("mathSymbol").value = "";
+  document.getElementById(`tokens`).value = ``;
+};
+document.querySelector(`form`).addEventListener(`submit`, entry);
