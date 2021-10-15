5d6e59c554b606461f97d8e8364b438a2e74e71c
Norman Li <normanjli@gmail.com>
2021-10-15 07:47:22 -0600

    solutions

--- assessments/assessment-front-end-web/about.js
+++ assessments/assessment-front-end-web/about.js
@@ -5,10 +5,15 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Thank you for your submission');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const complimentHover=()=>{
+	alert(`this cat loves your kindness!`)
+}
+document.querySelector(`img`).addEventListener(`mouseover`, complimentHover)
