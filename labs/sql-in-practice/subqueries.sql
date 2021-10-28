fb95635db9e106b907b76b3a4e79b17395e3ba24
Norman Li <normanjli@gmail.com>
2021-10-28 07:03:38 -0600

    commit for answers

--- labs/sql-in-practice/subqueries.sql
+++ labs/sql-in-practice/subqueries.sql
@@ -1,5 +1,41 @@
 SELECT * FROM invoice
 WHERE invoice_id in(
-    select * from invoice_line
+    select invoice_id from invoice_line
     where unit_price >.99
 )
+
+SELECT name FROM track
+where track_id in(
+    select track_id from playlist_track
+    where playlist_id in(
+        Select playlist_id from playlist
+        where name = 'Music'
+    )
+);
+
+SELECT name from track
+where track_id in(
+    select track_id from playlist_track
+    where playlist_id = 5
+);
+
+Select * from track
+where genre_id in(
+    Select genre_id from genre
+    where name = 'Comedy'
+);
+
+SELECT * from track
+where album_id in(
+    select album_id from album
+    where title = 'Fireball'
+);
+
+SELECT * from track
+where album_id in(
+    select album_id from album
+    where artist_id in(
+        SELECT artist_id FROM artist
+        where name = 'Queen'
+    )
+);
