4ababcf8f1348c7e2cf314d083f2b2d3e956abcf
Norman Li <normanjli@gmail.com>
2021-10-29 07:07:39 -0600

    initial, soltn to process.py

---
+++ assessments/assessment-data/process.py
@@ -0,0 +1,12 @@
+log_file = open("D:\devMountain\assessments\assessment-data\um-server-01.txt") #opens a file in the directory and stores the data in a variable
+
+
+def sales_reports(log_file): #function call
+    for line in log_file: #for loop looping opver each line in the open file
+        line = line.rstrip() #removing whitespace in each line
+        day = line[0:3] #sets the first three chracters of the line equal to a new variable day
+        if day == "Tue": #checkec the variable against day
+            print(line)#prints the line if it evals true
+
+
+sales_reports(log_file) #closes the open file
