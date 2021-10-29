5eb07d1c054ce8e1f12d0132041c2ae02e7e0a23
Norman Li <normanjli@gmail.com>
2021-10-29 08:33:53 -0600

    added extracredit to python file

--- assessments/assessment-data/process.py
+++ assessments/assessment-data/process.py
@@ -2,7 +2,7 @@ log_file = open(r'D:\devMountain\assessments\assessment-data\um-server-01.txt')
 #opens a file in the directory and stores the data in a variable
 
 
-def sales_reports(log_file): #function call
+def sales_reports(log_file): #function declaration
     for line in log_file: #for loop looping opver each line in the open file
         line = line.rstrip() #removing whitespace in each line
         day = line[0:3] #sets the first three chracters of the line equal to a new variable day
@@ -10,4 +10,11 @@ def sales_reports(log_file): #function call
             print(line)#prints the line if it evals true
 
 
-sales_reports(log_file) #closes the open file
+# sales_reports(log_file) #calls the function
+
+def ten_melon(log_file):
+    for line in log_file:
+        if float(line.split(' ')[2])>10:
+            print(line)
+
+ten_melon(log_file)
