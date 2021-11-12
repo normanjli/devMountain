0965d31ebf8c3232318316e126e55501d8b898a0
Norman Li <normanjli@gmail.com>
2021-11-12 11:43:55 -0700

    typo in answer

--- assessments/cs/cs.js
+++ assessments/cs/cs.js
@@ -1,23 +1,24 @@
 const addToZero = (nums) =>{
-    nums.sort((a,b)=>a-b)
-    let index1= 0
-    let index2=nums.length-1
-    while(index1!==index2){
-        if (nums[index1]+nums[index2]===0){
-            return true
+    nums.sort((a,b)=>a-b) //nlogn
+    let index1= 0 //1
+    let index2=nums.length-1//2
+    while(index1!==index2){//1n
+        if (nums[index1]+nums[index2]===0){//2
+            return true//1
         }
-        else if (nums[index1]+nums[index2]>0){
-            index2--
-        }else if(nums[index1]+nums[index2]<0){
-            index1++
+        else if (nums[index1]+nums[index2]>0){//2
+            index2--//1
+        }else if(nums[index1]+nums[index2]<0){//2
+            index1++//1
         }
     }
-    return false
+    return false//1
 }
 console.log(addToZero([2,-2,5,6]))
 //space complexity is o(n) time complexity is nlogn due to sort. if array was presorted it would be o(n)
+//nLog(n)+1+2+1N+2+1+2+1+2+1+1 = nLog(n)+N+13 lim as n approaches inf = nlog(n)
 function hasUniqueSet(str){
-    return new Set(str).size === str.length? true:false
+    return new Set(str).size === str.length? true:false//n(where n str len) +1+1+1+1+1
 }
 console.log(hasUniqueSet(`moonday`)) 
 // space complexity of O(n) timecomplexity of O(n) i believe the set construction is o(n)otherwise it would be o(1)
